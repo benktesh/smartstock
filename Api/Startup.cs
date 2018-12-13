6f68843cca83782183c0c3e9e92b2944f0fbd50e
benktesh <benktesh@yahoo.com>
2018-12-13 11:49:30 -0600

    clean up and adds xml documentation path in swager

--- Api/Startup.cs
+++ Api/Startup.cs
@@ -1,50 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using System.IO;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
 using Swashbuckle.AspNetCore.Swagger;
-
 
 namespace Api
 {
     public class Startup
     {
-        private string applicationName;
-        private string version; 
-
+        private string _applicationName;
+        private string _version;
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
         }
-
         public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
-
             var config = Configuration.GetSection("ApplicationSetting").Get<ApplicationSetting>();
-            applicationName = config.Name;
-            version = config.Version;
+            _applicationName = config.Name;
+            _version = config.Version;
 
-
-            // Register the Swagger generator
+            // Register the Swagger
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Info { Title = applicationName, Version = version });
+                c.SwaggerDoc(_version, new Info { Title = _applicationName, Version = _version });
+                // Set the comments path for the Swagger JSON and UI.
+                var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                c.IncludeXmlComments(xmlPath);
             });
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             if (env.IsDevelopment())
@@ -55,17 +48,15 @@ namespace Api
             {
                 app.UseHsts();
             }
-
-            // Enable middleware to serve generated Swagger as a JSON endpoint.
+           
+            // Enable Swagger JSON endpoint.
             app.UseSwagger();
-
-            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.), 
-            // specifying the Swagger JSON endpoint.
+            // Enable swagger-ui (HTML, JS, CSS, etc.), with the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
-                {
-                    c.SwaggerEndpoint("/swagger/v1/swagger.json", $"{applicationName} {version}");
+            {
+                c.SwaggerEndpoint($"/swagger/{_version}/swagger.json", $"{_applicationName} {_version}");
+                
             });
-
             app.UseHttpsRedirection();
             app.UseMvc();
         }
